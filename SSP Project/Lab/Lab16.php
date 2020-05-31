0776fe46b96990f8e6bd7fa1c79df89c5da0e306
Valetta Jesslyn <rureihl@yahoo.com.sg>
2020-05-31 21:27:58 +0800

    Revert "Minor changes"
    This reverts commit cf583d940a80c4910f68f310a0ecef5ed8ab8eeb.

--- SSP Project/Lab/Lab16.php
+++
@@ -1,5 +0,0 @@
-
-
-<form action='' method='post'>
-  <img src='Lab16-2.php'/>
-</form>
--- SSP Project/Other page/Login/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-  require_once $_SERVER['DOCUMENT_ROOT']."/SSP Project/core/init.php";
-  unset($_SESSION['userID']);
-  header("Location: login.php");
-?>
